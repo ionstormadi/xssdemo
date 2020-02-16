a719ad9280545c7e5503eaf6a833008c3e61fa22
Aditya <aditya@xss.com>
2020-02-16 22:48:45 +0530

    Initial commit of the XSS demo

---
+++ takeinput/src/com/dev/HelloServlet.java
@@ -0,0 +1,45 @@
+package com.dev;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/helloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String yourName = request.getParameter("yourName");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.close();
+	}
+	/**
+	public static void main(String[] args ) {
+		Object o = getObject();
+		Object o2 = o;
+		System.out.println(o2.toString());
+	}
+	private static Object getObject(){
+		return null;
+	}**/
+
+}
